274b413af6a03dfd016a79bf40862b136a9c1a98
Mal <qrednuht_go@outlook.com>
2021-11-01 20:19:37 -0700

    UI menu

---
+++ UI.py
@@ -0,0 +1,23 @@
+
+def option_selection(*args):
+    if len(args) == 0:
+        print('')
+    
+    for index, value in enumerate(args):
+        print(f'{index + 1}. Press {index + 1} to do {value}')
+    
+    user_choice = int(input('Please select an option: '))
+    
+    return args[user_choice - 1]
+    
+    
+
+
+
+if __name__ == "__main__":
+    inp = "Do Homework"
+    inp2 = "Study"
+    inp3 = "Eat"
+    choice = option_selection(inp, inp2, inp3)
+
+    print(f'You have selected {choice}')
