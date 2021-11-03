3ffd3e9e582935ea1aa0336bfa0a8f33dfd4f41d
Sarah Kwon <sarahkwon@umail.ucsb.edu>
2021-11-02 20:07:44 -0700

    simple stats and resources classes

---
+++ components/stats.py
@@ -0,0 +1,33 @@
+class Stats:
+    subjectKnowledge
+    charisma 
+    physical
+
+    def __init__():
+        subjectKnowledge = 0
+        charisma = 0
+        physical = 0
+
+    def raiseSubjectKnowledge(amt):
+        subjectKnowledge += amt
+
+    def lowerSubjectKnowledge(amt):
+        subjectKnowledge -= amt
+        if (subjectKnowledge < 0)
+            subjectKnowledge = 0
+
+    def raiseCharisma(amt):
+        charisma += amt
+
+    def lowerCharisma(amt):
+        charisma -= amt
+        if (charisma < 0)
+            charisma = 0
+
+    def raisePhysical(amt):
+        physical += amt
+
+    def lowerPhysical(amt):    
+        physical -= amt
+        if (physical < 0)
+            physical = 0
