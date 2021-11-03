6eb7b5e4f299beee3ab8ea8f62b9c0f8222979cd
Eeriedescent <56623944+Eeriedescent@users.noreply.github.com>
2021-11-02 20:24:45 -0700

    Add files via upload

---
+++ Protagonist_class.py
@@ -0,0 +1,17 @@
+#protagonist.py'
+
+class Protagonist():
+    def __init__(self, your_name, your_major):
+        self.name = your_name
+        self.major = your_major
+        self.s_Knowledge = 0
+        self.s_Charisma = 5
+        self.s_Fitness = 1
+        self.s_XP = 1
+        self.r_stress = 0
+        self.r_energy = 100
+        self.r_money = 50
+
+
+
+
