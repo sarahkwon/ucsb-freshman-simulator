a2b3b203027bb18df98e2b3766ac143b0e058aeb
Mal <56319809+Moo-ware@users.noreply.github.com>
2021-11-03 19:58:33 -0700

    Update UI.py

--- UI.py
+++ UI.py
@@ -1,14 +1,17 @@
-
-def option_selection(*args):
-    if len(args) == 0:
-        print('')
-    
-    for index, value in enumerate(args):
-        print(f'{index + 1}. Press {index + 1} to do {value}')
-    
-    user_choice = int(input('Please select an option: '))
-    
-    return args[user_choice - 1]
+class ui():
+    def __init__(self):
+        pass
+       
+    def option_selection(*args):
+        if len(args) == 0:
+            print('')
+        
+        for index, value in enumerate(args):
+            print(f'{index + 1}. Press {index + 1} to do {value}')
+        
+        user_choice = int(input('Please select an option: '))
+        
+        return args[user_choice - 1]
     
     
 
@@ -18,6 +21,7 @@ if __name__ == "__main__":
     inp = "Do Homework"
     inp2 = "Study"
     inp3 = "Eat"
-    choice = option_selection(inp, inp2, inp3)
+    ui1 = ui()
+    choice = ui1.option_selection(inp, inp2, inp3)
 
     print(f'You have selected {choice}')
