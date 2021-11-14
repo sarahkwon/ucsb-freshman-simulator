ed6639934b28651e1e0dc109380afa2569ff4cce
Sarah Kwon <sarahkwon@umail.ucsb.edu>
2021-11-14 14:18:20 -0800

    updated activities and events parameters

--- ClassActivity.py
+++ ClassActivity.py
@@ -2,16 +2,25 @@ import csv
 
 class ClassActivity:
 
-    def __init__(self, name, desc, timeCost, statModifiers, resourceModifiers, flavorText):
+    def __init__(self, name, desc, timeCost, intelligence, charisma, fitness, resourceModifiers, flavorText):
         self.name = name
         self.desc = desc
         self.timeCost = timeCost
-        self.statModifiers = statModifiers
+        self.intelligence = intelligence
+        self.charisma = charisma
+        self.fitness = fitness
         self.resourceModifiers = resourceModifiers
         self.flavorText = flavorText
     
     def __repr__(self):
-        return "Name: " + self.name + ": \n" + "Description: " + self.desc + "\n" + "Time Cost: " + str(self.timeCost) + "\n" + "Stat Modifiers: " + self.statModifiers + "\n" +"Resource Modifiers: " + self.resourceModifiers + "\n" +"Flavor Text: " + self.flavorText + "\n"
+        return "Name: " + self.name + ": \n" + 
+        "Description: " + self.desc + "\n" + 
+        "Time Cost: " + str(self.timeCost) + "\n" + 
+        "Intelligence: " + str(self.intelligence) + "\n" + 
+        "Charisma: "+ str(self.charisma) + "\n" + 
+        "Fitness: " + str(self.fitness) + "\n" +
+        "Resource Modifiers: " + self.resourceModifiers + "\n" +
+        "Flavor Text: " + self.flavorText + "\n"
 
 def addActivities(fileName):
         activities = {}
@@ -21,13 +30,15 @@ def addActivities(fileName):
             i = 0
             for row in r:
                 # for each row in csvFile, append the class activity to the array activities
-                activities[row["name"]] = ClassActivity(
-                    str(row["name"]), 
-                    str(row["description"]), 
-                    int(row["timeCost"]), 
-                    str(row["statModifiers"]), 
-                    str(row["resourceModifiers"]),
-                    str(row["flavor"])
+                activities[row["Name"]] = ClassActivity(
+                    str(row["Name"]), 
+                    str(row["Description"]), 
+                    int(row["Time Cost"]), 
+                    str(row["Intelligence"]),
+                    str(row["Charisma"]),
+                    str(row["Fitness"]), 
+                    str(row["Resource Modifiers"]),
+                    str(row["Flavor Text"])
                 )
                 i += 1
         return activities
