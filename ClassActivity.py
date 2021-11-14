53350388fc90ce7617b967efab49aef5a0c5b6d9
Sarah Kwon <sarahkwon@umail.ucsb.edu>
2021-11-14 15:38:11 -0800

    fixed classacitivity

--- ClassActivity.py
+++ ClassActivity.py
@@ -16,7 +16,7 @@ class ClassActivity:
         self.flavorText = flavorText
     
     def __repr__(self):
-        return "Name: " + self.name + ": \n" + "Description: " + self.desc + "\n" + "Time Cost: " + str(self.timeCost) + "\n" + "Intelligence: " + str(self.intelligence) + "\n" + "Charisma: "+ str(self.charisma) + "\n" + "Fitness: " + str(self.fitness) + "\n" +"Resource Modifiers: " + self.resourceModifiers + "\n" +"Flavor Text: " + self.flavorText + "\n"
+        return "Name: " + self.name + ": \n" + "Description: " + self.desc + "\n" + "Time Cost: " + str(self.timeCost) + "\n" + "Intelligence: " + str(self.intelligence) + "\n" + "Charisma: "+ str(self.charisma) + "\n" + "Fitness: " + str(self.fitness) + "Stress: " + self.stress + "\n" + "Energy: " + self.energy + "\n" + "Money: " + self.money + "\n" +"Flavor Text: " + self.flavorText + "\n"
 
 def addActivities(fileName):
         activities = {}
@@ -30,17 +30,18 @@ def addActivities(fileName):
                     str(row["Code"]),
                     str(row["Name"]), 
                     str(row["Description"]), 
-                    int(row["Time Cost"]), 
-                    int(row["Intelegence"]),
-                    int(row["Charisma"]),
-                    int(row["Fitness"]), 
-                    int(row["Energy"]),
-                    int(row["Stress"]),
-                    int(row["Money"]),
+                    str(row["Time Cost"]), 
+                    str(row["Intelegence"]),
+                    str(row["Charisma"]),
+                    str(row["Fitness"]), 
+                    str(row["Energy"]),
+                    str(row["Stress"]),
+                    str(row["Money"]),
                     str(row["Flavor Text"])
                 )
                 i += 1
         return activities
 
 if __name__ == "__main__":
-    
+    a = addActivities('test.csv')
+    print(a)
