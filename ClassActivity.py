7c36cc2c69c025b7310b0423fe22029f426ada19
Sarah Kwon <sarahkwon@umail.ucsb.edu>
2021-11-15 17:23:54 -0800

    updated csv file formatting and classActivity now works

--- ClassActivity.py
+++ ClassActivity.py
@@ -16,13 +16,13 @@ class ClassActivity:
         self.flavorText = flavorText
     
     def __repr__(self):
-        return "Name: " + self.name + ": \n" + "Description: " + self.desc + "\n" + "Time Cost: " + str(self.timeCost) + "\n" + "Intelligence: " + str(self.intelligence) + "\n" + "Charisma: "+ str(self.charisma) + "\n" + "Fitness: " + str(self.fitness) + "\n" + "Stress: " + self.stress + "\n" + "Energy: " + self.energy + "\n" + "Money: " + self.money + "\n" +"Flavor Text: " + self.flavorText + "\n"
+        return "Name: " + self.name + ": \n" + "Description: " + self.desc + "\n" + "Time Cost: " + str(self.timeCost) + "\n" + "Intelligence: " + str(self.intelligence) + "\n" + "Charisma: "+ str(self.charisma) + "\n" + "Fitness: " + str(self.fitness) + "\n" + "Energy: " + str(self.energy) + "\n" + "Stress: " + str(self.stress) + "\n" + "Money: " + str(self.money) + "\n" +"Flavor Text: " + self.flavorText + "\n\n"
 
 def addActivities(fileName):
         activities = {}
         # open the csv file fileName and read from it
-        with open(fileName, newline = '') as csvfile:
-            r = csv.DictReader(csvfile, delimiter=',')
+        with open(fileName, encoding="utf8", newline = '') as csvfile:
+            r = csv.DictReader(csvfile, delimiter=',', quoting=csv.QUOTE_ALL, skipinitialspace=True)
             i = 0
             for row in r:
                 # for each row in csvFile, append the class activity to the array activities
@@ -30,13 +30,13 @@ def addActivities(fileName):
                     str(row["Code"]),
                     str(row["Name"]), 
                     str(row["Description"]), 
-                    str(row["Time Cost"]), 
-                    str(row["Intelligence"]),
-                    str(row["Charisma"]),
-                    str(row["Fitness"]), 
-                    str(row["Energy"]),
-                    str(row["Stress"]),
-                    str(row["Money"]),
+                    int(row["Time Cost"]), 
+                    int(row["Intelligence"]),
+                    int(row["Charisma"]),
+                    int(row["Fitness"]), 
+                    int(row["Energy"]),
+                    int(row["Stress"]),
+                    int(row["Money"]),
                     str(row["Flavor Text"])
                 )
                 i += 1
