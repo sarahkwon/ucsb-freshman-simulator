1e1852afa8c437d80b5e50108092a7fb1b536cd7
Sarah Kwon <sarahkwon@umail.ucsb.edu>
2021-11-20 01:58:35 -0800

    changed knowledge to intelligence, updated protag class variable naming

--- Protagonist_class.py
+++ Protagonist_class.py
@@ -6,13 +6,13 @@ class Protagonist():
     def __init__(self, your_name, your_major):
         self.name = your_name
         self.major = your_major
-        self.s_Intelligence = 0
-        self.s_Charisma = 5
-        self.s_Fitness = 1
-        self.s_XP = 1 
-        self.r_stress = 0 #0 - 100%
-        self.r_energy = 100 #0 - 100%
-        self.r_money = 50 
+        self.intelligence = 0
+        self.charisma = 5
+        self.fitness = 1
+        self.xp = 1 
+        self.stress = 0 #0 - 100%
+        self.energy = 100 #0 - 100%
+        self.money = 50 
 
     # update player class given a class activity
     def updateStats(self, activity):
@@ -33,46 +33,46 @@ class Protagonist():
 
     # basic set functions for class    
     def updateIntelligence(self, amt):
-        self.s_Intelligence += amt
-        if (self.s_Intelligence < 0):
-            self.s_Intelligence = 0
+        self.intelligence += amt
+        if (self.intelligence < 0):
+            self.intelligence = 0
 
     def updateCharisma(self, amt):
-        self.s_Charisma += amt
-        if (self.s_Charisma < 0):
-            self.s_Charisma = 0
+        self.charisma += amt
+        if (self.charisma < 0):
+            self.charisma = 0
 
     def updateFitness(self, amt):
-        self.s_Fitness += amt
-        if (self.s_Fitness < 0):
-            self.s_Fitness = 0
+        self.fitness += amt
+        if (self.fitness < 0):
+            self.fitness = 0
 
     def updateXP(self, amt):
-        self.s_XP += amt
+        self.xp += amt
 
     def updateStress(self, amt):
-        self.r_stress += amt
-        if (self.r_stress > 100):
-            self.r_stress = 100
-        if (self.r_stress < 0):
-            self.r_stress = 0
+        self.stress += amt
+        if (self.stress > 100):
+            self.stress = 100
+        if (self.stress < 0):
+            self.stress = 0
 
     def updateEnergy(self, amt):
-        self.r_energy += amt
-        if (self.r_energy > 100):
-            self.r_energy = 100
-        if (self.r_energy < 0):
-            self.r_energy = 0
+        self.energy += amt
+        if (self.energy > 100):
+            self.energy = 100
+        if (self.energy < 0):
+            self.energy = 0
 
     def updateMoney(self, amt):
-        self.r_money += amt
+        self.money += amt
 
 if __name__ == "__main__":
     test = Protagonist("John", "cs")
     testActivity = Activity.ClassActivity("test_code", "test_activity", "this is a test activity", 1, 1, 1,-1, -1, -1, "flavor text")
     test.updateStats(testActivity)
-    print(test.r_energy)
+    print(test.stress)
 
     testEvent = Event.ClassEvent("test_event", "test event name", "this is a test event", 1, 1, 1, 1, 1, 1, 1, 4, -1, -1, -1, -1, "flavor text")
     test.updateStats(testEvent)
-    print(test.s_Intelligence)
+    print(test.intelligence)
