3a579510249abcea5ee9b0162b82cf5d57497ccd
= <=>
2021-11-20 02:08:15 -0800

    PrintStats for protagonist class; testing file expansion

--- Protagonist_class.py
+++ Protagonist_class.py
@@ -5,13 +5,13 @@ class Protagonist():
     def __init__(self, your_name, your_major):
         self.name = your_name
         self.major = your_major
-        self.s_Knowledge = 0
-        self.s_Charisma = 5
-        self.s_Fitness = 1
-        self.s_XP = 1 
-        self.r_stress = 0 #0 - 100%
-        self.r_energy = 100 #0 - 100%
-        self.r_money = 50 
+        self.intelligence = 0
+        self.charisma = 5
+        self.fitness = 1
+        self.xp = 1 
+        self.stress = 0 #0 - 100%
+        self.energy = 100 #0 - 100%
+        self.money = 50
 
     # update player class given a class activity
     def updateStats(self, activity):
@@ -25,39 +25,57 @@ class Protagonist():
 
     # basic set functions for class    
     def updateKnowledge(self, amt):
-        self.s_Knowledge += amt
-        if (self.s_Knowledge < 0):
-            self.s_Knowledge = 0
+        self.knowledge += amt
+        if (self.knowledge < 0):
+            self.knowledge = 0
 
     def updateCharisma(self, amt):
-        self.s_Charisma += amt
-        if (self.s_Charisma < 0):
-            self.s_Charisma = 0
+        self.charisma += amt
+        if (self.charisma < 0):
+            self.charisma = 0
 
     def updateFitness(self, amt):
-        self.s_Fitness += amt
-        if (self.s_Fitness < 0):
-            self.s_Fitness = 0
+        self.fitness += amt
+        if (self.fitness < 0):
+            self.fitness = 0
 
     def updateXP(self, amt):
-        self.s_XP += amt
+        self.xp += amt
 
     def updateStress(self, amt):
         self.r_stress += amt
-        if (self.r_stress > 100):
-            self.r_stress = 100
-        if (self.r_stress < 0):
-            self.r_stress = 0
+        if (self.stress > 100):
+            self.stress = 100
+        if (self.stress < 0):
+            self.stress = 0
 
     def updateEnergy(self, amt):
-        self.r_energy += amt
-        if (self.r_energy > 100):
-            self.r_energy = 100
-        if (self.r_energy < 0):
-            self.r_energy = 0
+        self.energy += amt
+        if (self.energy > 100):
+            self.energy = 100
+        if (self.energy < 0):
+            self.energy = 0
 
     def updateMoney(self, amt):
-        self.r_money += amt
+        self.money += amt
+
+    def printStats(self):
+        print(
+            """
+    {}
+    Major: {}
+|----RESOURCES----<>
+| Stress   | {}   
+| Energy   | {}   
+| Money    | ${}
+|------STATS------<>
+| XP       | {} 
+| Intel    | {}   
+| Fitness  | {}   
+| Charisma | {}   
+|------------------<>
+            """.format(self.name, self.major, self.stress, self.energy, self.money, self.xp, self.intelligence, self.fitness, self.charisma)
+        )
 
 if __name__ == "__main__":
     test = Protagonist("John", "cs")
