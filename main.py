a852f9c0533fcd55d64cb32d0ddb0979cae437c2
Mal <56319809+Moo-ware@users.noreply.github.com>
2021-11-20 19:09:56 -0800

    Added comments

--- main.py
+++ main.py
@@ -27,20 +27,26 @@ def main_loop():
             break
         
 
-
+        #adds all the activity names to a list so it can be passed through UI    
         activities_list = []
         for i in activities:
             activities_list.append(activities[i].name)
+        #adds all the event names to a list so it can be passed through UI 
         events_list = []
         for i in event.events:
             events_list.append(event.events[i].name)
 
+        #creates an UI menu based on the names of activities and events
+        #performs the action user has selected
+        #returns the choice of user's selection in code form
         choice = ui.option_selection(activities_list, events_list)
-        print(choice)
         
+        #Exits the loop if user choice is "z" or "Z".
         if choice == 'z':
+            print("Goodbye")
             break
-
+        
+        print("Press Enter to continue.")
 
 
     return None
